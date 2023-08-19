4e6958a72e705f1aa8a502c5c0eebf70e34d9418
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-08-19 12:57:31 +0300

    Update

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 exports.SECRET = 'somerandomsecret';
-exports.DB_URI = 'mongodb+srv://dummyUser:abcd1234@cluster0.vc74lje.mongodb.net/?retryWrites=true&w=majority';
+exports.DB_URI = 'mongodb+srv://dummyUser:abcd1234@cluster0.vc74lje.mongodb.net/?retryWrites=true&w=majority/forum';
